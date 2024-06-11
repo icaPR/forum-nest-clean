7b378eb2ab0f86644e9cc0a7d9f625bd811b214e
Icaro Ramos <polosnramos@hotmail.com>
2024-06-11 17:46:13 -0300

    feat: create JwtStrategy to secure routes for authenticated users

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { ConfigService } from "@nestjs/config";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { Env } from "src/env";
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
   imports: [
@@ -21,5 +22,6 @@ import { Env } from "src/env";
       },
     }),
   ],
+  providers: [JwtStrategy],
 })
 export class AuthModule {}
