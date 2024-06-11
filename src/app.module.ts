ca4a31f0c2e93fa7d1b08d03409d3e2f7f6e1386
Icaro Ramos <polosnramos@hotmail.com>
2024-06-11 18:57:55 -0300

    feat: create CreateQuestionController

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,8 @@ import { CreateAccountController } from "./controllers/create-account.controller
 import { ConfigModule } from "@nestjs/config";
 import { envSchema } from "./env";
 import { AuthModule } from "./auth/auth.module";
-import { AuthenticateController } from "./controllers/authenticate";
+import { AuthenticateController } from "./controllers/authenticate.controller";
+import { CreateQuestionController } from "./controllers/create-question.controller";
 
 @Module({
   imports: [
@@ -14,7 +15,11 @@ import { AuthenticateController } from "./controllers/authenticate";
     }),
     AuthModule,
   ],
-  controllers: [CreateAccountController, AuthenticateController],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,53 @@
+import { Body, UseGuards, UsePipes } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { AuthGuard } from "@nestjs/passport";
+import { CurrentUser } from "src/auth/current-user-decoratort";
+import { TokenPayloadSchema } from "src/auth/jwt.strategy";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
+
+@Controller()
+@UseGuards(AuthGuard("jwt"))
+export class CreateQuestionController {
+  constructor(private prisma: PrismaService, private jwt: JwtService) {}
+  @Post("/questions")
+  async handle(
+    @Body(new ZodValidationPipe(createQuestionBodySchema))
+    body: CreateQuestionBodySchema,
+    @CurrentUser() user: TokenPayloadSchema
+  ) {
+    const { title, content } = body;
+    const userId = user.sub;
+
+    const slug = this.convertToSlug(title);
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      },
+    });
+
+    return "ok";
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/[^\w\s-]/g, "")
+      .replace(/\s+/g, "-");
+  }
+}
