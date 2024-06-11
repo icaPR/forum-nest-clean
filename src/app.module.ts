5851d554af57c33b342e5a3d8241e633702f88c5
Icaro Ramos <polosnramos@hotmail.com>
2024-06-11 10:38:21 -0300

    feat: create environment variables validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma/prisma.service";
 import { CreateAccountController } from "./controllers/create-account.controller";
+import { ConfigModule } from "@nestjs/config";
+import { envSchema } from "./env";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
