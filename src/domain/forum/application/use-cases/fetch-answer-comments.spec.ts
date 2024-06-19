05b55daf757544b06a6687440e0188f2636fe9a0
Icaro Ramos <polosnramos@hotmail.com>
2024-06-19 04:34:15 -0300

    test: update tests to verify FetchAnswerCommentsUseCase returns CommentWithAuthor

--- src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
+++ src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
@@ -2,39 +2,63 @@ import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-answer-comment-repository";
 import { FetchAnswerCommentsUseCase } from "./fetch-answer-comments";
 import { makeAnswerComment } from "test/factories/make-answer-comment";
+import { InMemoryStudentsRepository } from "test/repositories/in-memory-students-repository";
+import { makeStudent } from "test/factories/make-student";
 
 let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository;
+let inMemoryStudentsRepository: InMemoryStudentsRepository;
 let sut: FetchAnswerCommentsUseCase;
 
 describe("Fetch answer comments", async () => {
   beforeEach(() => {
-    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository();
+    inMemoryStudentsRepository = new InMemoryStudentsRepository();
+
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository(
+      inMemoryStudentsRepository
+    );
     sut = new FetchAnswerCommentsUseCase(inMemoryAnswerCommentsRepository);
   });
 
   it("should be able to fetch answer comments", async () => {
-    await inMemoryAnswerCommentsRepository.create(
-      makeAnswerComment({ answerId: new UniqueEntityID("answer1") })
-    );
-    await inMemoryAnswerCommentsRepository.create(
-      makeAnswerComment({ answerId: new UniqueEntityID("answer2") })
-    );
-    await inMemoryAnswerCommentsRepository.create(
-      makeAnswerComment({ answerId: new UniqueEntityID("answer1") })
-    );
+    const student = makeStudent({ name: "Test name" });
+    inMemoryStudentsRepository.item.push(student);
+
+    const coment1 = makeAnswerComment({
+      answerId: new UniqueEntityID("answer1"),
+      authorId: student.id,
+    });
+
+    const coment2 = makeAnswerComment({
+      answerId: new UniqueEntityID("answer2"),
+      authorId: student.id,
+    });
+
+    const coment3 = makeAnswerComment({
+      answerId: new UniqueEntityID("answer1"),
+      authorId: student.id,
+    });
+
+    await inMemoryAnswerCommentsRepository.create(coment1);
+    await inMemoryAnswerCommentsRepository.create(coment2);
+    await inMemoryAnswerCommentsRepository.create(coment3);
 
     const result = await sut.handle({
       answerId: "answer1",
       page: 1,
     });
 
-    expect(result.value?.answerComments).toHaveLength(2);
+    expect(result.value?.comments).toHaveLength(2);
   });
 
   it("should be able to fetch pagination answer comments", async () => {
+    const student = makeStudent({ name: "Test name" });
+    inMemoryStudentsRepository.item.push(student);
     for (let i = 1; i <= 22; i++) {
       await inMemoryAnswerCommentsRepository.create(
-        makeAnswerComment({ answerId: new UniqueEntityID("answer1") })
+        makeAnswerComment({
+          answerId: new UniqueEntityID("answer1"),
+          authorId: student.id,
+        })
       );
     }
 
@@ -42,6 +66,6 @@ describe("Fetch answer comments", async () => {
       page: 1,
       answerId: "answer1",
     });
-    expect(result.value?.answerComments).toHaveLength(20);
+    expect(result.value?.comments).toHaveLength(20);
   });
 });
--- test/repositories/in-memory-answer-comment-repository.ts
+++ test/repositories/in-memory-answer-comment-repository.ts
@@ -1,11 +1,46 @@
 import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
 import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
+import { CommentWithAuthor } from "@/domain/forum/enterprise/entities/value-object/comment-with-authr";
+import { InMemoryStudentsRepository } from "./in-memory-students-repository";
 
 export class InMemoryAnswerCommentsRepository
   implements AnswerCommentsRepository
 {
   public item: AnswerComment[] = [];
+
+  constructor(private inMemoryStudentsRepository: InMemoryStudentsRepository) {}
+
+  async findManyByAnswerIdWithAuthor(
+    answerId: string,
+    { page }: PaginationParams
+  ) {
+    const answerComments = this.item
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+      .map((comment) => {
+        const author = this.inMemoryStudentsRepository.item.find((student) => {
+          return student.id.equals(comment.authorId);
+        });
+
+        if (!author) {
+          throw new Error(
+            `Author with ID "${comment.authorId.toString()} does not exist."`
+          );
+        }
+
+        return CommentWithAuthor.create({
+          commentId: comment.id,
+          content: comment.content,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+          authorId: comment.authorId,
+          author: author.name,
+        });
+      });
+
+    return answerComments;
+  }
 
   async create(answerComment: AnswerComment) {
     this.item.push(answerComment);
