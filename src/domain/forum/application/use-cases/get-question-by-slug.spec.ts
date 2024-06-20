a0cca1ae7033f7c8e414868431040bf02b23145b
Icaro Ramos <polosnramos@hotmail.com>
2024-06-19 23:35:58 -0300

    test: update tests to verify returns QuestionDetails

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -3,26 +3,54 @@ import { GetQuestionBySlugUseCase } from "./get-question-by-slug";
 import { makeQuestion } from "test/factories/make-questions";
 import { InMemoryQuestionAttachmentRepository } from "test/repositories/in-memory-question-attachment-repository";
 import { Slug } from "../../enterprise/entities/value-object/slug";
+import { InMemoryAttachmentsRepository } from "test/repositories/in-memory-attachments-repository";
+import { InMemoryStudentsRepository } from "test/repositories/in-memory-students-repository";
+import { makeStudent } from "test/factories/make-student";
+import { makeAttachment } from "test/factories/make-attachment";
+import { makeQuestionAttachment } from "test/factories/make-question-attachment";
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let inMemoryQuestionAttachmentRepository: InMemoryQuestionAttachmentRepository;
+let inMemoryAttachmentsRepository: InMemoryAttachmentsRepository;
+let inMemoryStudentsRepository: InMemoryStudentsRepository;
 let sut: GetQuestionBySlugUseCase;
 
 describe("Get question by slug", async () => {
   beforeEach(() => {
     inMemoryQuestionAttachmentRepository =
       new InMemoryQuestionAttachmentRepository();
+    inMemoryAttachmentsRepository = new InMemoryAttachmentsRepository();
+    inMemoryStudentsRepository = new InMemoryStudentsRepository();
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
-      inMemoryQuestionAttachmentRepository
+      inMemoryQuestionAttachmentRepository,
+      inMemoryAttachmentsRepository,
+      inMemoryStudentsRepository
     );
     sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository);
   });
 
   it("should be able to get a question by slug ", async () => {
+    const student = makeStudent({ name: "Test name" });
+
+    inMemoryStudentsRepository.item.push(student);
+
     const newQuestion = makeQuestion({
+      authorId: student.id,
       slug: Slug.create("example-question"),
     });
+
     await inMemoryQuestionsRepository.create(newQuestion);
+
+    const attachment = makeAttachment({ title: "Test attachment" });
+
+    await inMemoryAttachmentsRepository.create(attachment);
+
+    inMemoryQuestionAttachmentRepository.item.push(
+      makeQuestionAttachment({
+        attachmentId: attachment.id,
+        questionId: newQuestion.id,
+      })
+    );
 
     const result = await sut.handle({ slug: "example-question" });
 
@@ -30,6 +58,8 @@ describe("Get question by slug", async () => {
     expect(result.value).toMatchObject({
       question: expect.objectContaining({
         title: newQuestion.title,
+        author: "Test name",
+        attachments: [expect.objectContaining({ title: "Test attachment" })],
       }),
     });
   });
--- src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
+++ src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { AttachmentFactory } from "test/factories/make-attachment";
 import { INestApplication } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { Test } from "@nestjs/testing";
@@ -7,35 +8,53 @@ import { DatabaseModule } from "@/infra/database/database.module";
 import { Slug } from "@/domain/forum/enterprise/entities/value-object/slug";
 import { StudentFactory } from "test/factories/make-student";
 import { QuestionFactory } from "test/factories/make-questions";
+import { QuestionAttachmentFactory } from "test/factories/make-question-attachment";
 
 describe("Get question by slug controller - E2E", () => {
   let app: INestApplication;
   let studentFactory: StudentFactory;
   let questionFactory: QuestionFactory;
+  let attachmentFactory: AttachmentFactory;
+  let questionAttachmentFactory: QuestionAttachmentFactory;
   let jwt: JwtService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory, QuestionFactory],
+      providers: [
+        StudentFactory,
+        QuestionFactory,
+        AttachmentFactory,
+        QuestionAttachmentFactory,
+      ],
     }).compile();
 
     app = moduleRef.createNestApplication();
     studentFactory = moduleRef.get(StudentFactory);
+    attachmentFactory = moduleRef.get(AttachmentFactory);
+    questionAttachmentFactory = moduleRef.get(QuestionAttachmentFactory);
     questionFactory = moduleRef.get(QuestionFactory);
     jwt = moduleRef.get(JwtService);
     await app.init();
   });
 
   test("[GET] /questions/:slug", async () => {
-    const user = await studentFactory.makePrismaStudent();
+    const user = await studentFactory.makePrismaStudent({ name: "user1" });
 
     const accessToken = jwt.sign({ sub: user.id.toString() });
 
-    await questionFactory.makePrismaQuestion({
+    const question = await questionFactory.makePrismaQuestion({
       title: "question1",
       authorId: user.id,
       slug: Slug.create("question1"),
+    });
+    const attachment = await attachmentFactory.makePrismaAttachment({
+      title: "attachment1",
+    });
+
+    await questionAttachmentFactory.makePrismaQuestionAttachment({
+      attachmentId: attachment.id,
+      questionId: question.id,
     });
 
     const response = await request(app.getHttpServer())
@@ -45,7 +64,15 @@ describe("Get question by slug controller - E2E", () => {
 
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual({
-      questions: expect.objectContaining({ title: "question1" }),
+      question: expect.objectContaining({
+        title: "question1",
+        authorName: "user1",
+        attachments: [
+          expect.objectContaining({
+            title: "attachment1",
+          }),
+        ],
+      }),
     });
   });
 });
