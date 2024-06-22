b3da56a1ad55a163dab3ffc105b0d68da8ed6df6
Icaro Ramos <polosnramos@hotmail.com>
2024-06-22 16:38:27 -0300

    feat: update PrismaQuestionsRepository to save data in cache

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -7,11 +7,13 @@ import { PrismaService } from "../prisma.service";
 import { PrismaQuestionMapper } from "../mappers/prisma-question-mapper";
 import { PrismaQuestionDetailsMapper } from "../mappers/prisma-question-details-mapper";
 import { DomainEvents } from "@/core/events/domain-events";
+import { CacheRepository } from "@/infra/cache/cache-repository";
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
   constructor(
     private prisma: PrismaService,
+    private cacheRepository: CacheRepository,
     private questionAttachmentRepository: QuestionAttachmentRepository
   ) {}
 
@@ -36,6 +38,12 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
   }
 
   async findDetailsBySlug(slug: string) {
+    const cacheHit = await this.cacheRepository.get(`question:${slug}:details`);
+
+    if (cacheHit) {
+      const cachedData = JSON.parse(cacheHit);
+      return PrismaQuestionDetailsMapper.toDomain(cachedData);
+    }
     const question = await this.prisma.question.findUnique({
       where: { slug },
       include: {
@@ -46,7 +54,15 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     if (!question) {
       return null;
     }
-    return PrismaQuestionDetailsMapper.toDomain(question);
+
+    await this.cacheRepository.set(
+      `question:${slug}:details`,
+      JSON.stringify(question)
+    );
+
+    const questionDetails = PrismaQuestionDetailsMapper.toDomain(question);
+
+    return questionDetails;
   }
 
   async findById(questionId: string): Promise<Question | null> {
@@ -91,6 +107,8 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
       await this.questionAttachmentRepository.deleteMany(
         question.attachments.getRemovedItems()
       ),
+
+      this.cacheRepository.delete(`question:${data.slug}:details`),
     ]);
     DomainEvents.dispatchEventsForAggregate(question.id);
   }
